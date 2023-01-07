4220e508f40d207f479e73471d78599b6244b863
swordgeo <swordgeo1094@yahoo.com>
2023-01-07 15:36:17 -0500

    Testing

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
 const routes = require('express').Router();
 
+const {getContacts} = require('../controllers/user');
+
+
+
 
 //routes.get('/', jokes.displayJoke);
-routes.get('/', (req, res) => {
-  res.send('Jay McManus')
-});
-routes.get('/homie2', (req, res) => {
-  res.send('Ross Nichols')
-});
-routes.get('/homie3', (req, res) => {
-  res.send('Trevor Opdycke')
-});
+// routes.get('/', (req, res) => {
+//   res.send('Jay McManus')
+// });
+// routes.get('/homie2', (req, res) => {
+//   res.send('Ross Nichols')
+// });
+// routes.get('/homie3', (req, res) => {
+//   res.send('Trevor Opdycke')
+// });
+
+routes.get('/', getContacts);
+//routes.get('/username', userController.getUsername);
 
 module.exports = routes;
